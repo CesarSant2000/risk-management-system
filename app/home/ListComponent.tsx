839e793601593d17865190112ddac1e9274e2ab7
Cesar J. Santacruz <cesarjsantacruz2000@gmail.com>
2023-03-03 14:08:07 -0500

    Update ListComponent.tsx

--- app/home/ListComponent.tsx
+++ app/home/ListComponent.tsx
@@ -10,7 +10,7 @@ import {
     TextField,
     Typography
 } from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import IconButton from "@mui/material/IconButton";
@@ -30,8 +30,12 @@ export default function ListComponent(props: { title: JSX.Element, intro: JSX.El
     const {title, intro, array} = props;
     const [openDialog, setOpenDialog] = useState(false);
     const [dialogTitle, setDialogTitle] = useState('');
+    const [openDialog2, setOpenDialog2] = useState(false);
+    const [dialogTitle2, setDialogTitle2] = useState('');
+    const [dialogMessage2, setDialogMessage2] = useState('');
     const [commentaryHistory, setCommentaryHistory] = useState([] as CommentaryInterface[]);
     const [currentOptionIndex, setCurrentOptionIndex] = useState(0);
+    const [commentaryRecords, setCommentaryRecords] = useState([] as JSX.Element[]);
     const handleToggle = (value: number) => () => {
         const currentIndex = checked.indexOf(value);
         const newChecked = [...checked];
@@ -45,10 +49,43 @@ export default function ListComponent(props: { title: JSX.Element, intro: JSX.El
         setChecked(newChecked);
     };
 
+    useEffect(
+        () => {
+            if (checked.length === array.length + 1) {
+                setDialogTitle2(`Sección completada`);
+                setDialogMessage2(`Usted ha completado todos los pasos de esta sección.`);
+                setOpenDialog2(true);
+                const returnList: JSX.Element[] = [];
+                let tempIndex = 1;
+                if (commentaryHistory.length > 0) {
+                    returnList.push(
+                        <Typography variant="body1" color="text.primary">
+                            Recuerde que tiene los siguientes comentarios:
+                        </Typography>
+                    )
+                    returnList.push(<Divider/>)
+                    commentaryHistory.forEach((commentary) => {
+                            returnList.push(
+                                <Typography variant="body2" color="text.secondary">
+                                    {tempIndex}.- {commentary.commentary}
+                                </Typography>
+                            );
+                            tempIndex++;
+                        }
+                    )
+                    returnList.push(<Divider/>)
+                }
+                setCommentaryRecords(returnList);
+            }
+        }, [checked, commentaryHistory, array.length]
+    )
+
     function handleDialogClose() {
         //get input value from id commentary_text
+        const commentaryInput = document.getElementById('commentary_text') as HTMLInputElement;
+        const commentaryValue = commentaryInput.value;
         const commentary = {
-            commentary: document.getElementById('commentary_text')?.value,
+            commentary: commentaryValue,
             index: currentOptionIndex
         }
         if (commentary.commentary) {
@@ -57,12 +94,16 @@ export default function ListComponent(props: { title: JSX.Element, intro: JSX.El
         setOpenDialog(false);
     }
 
+    function handleDialogClose2() {
+        setOpenDialog2(false);
+    }
+
     function handleCommentary(index: number) {
         console.log("Comentario");
         setDialogTitle(`Ingrese un comentario para el punto ${index}`);
         setOpenDialog(true);
         setCurrentOptionIndex(index);
-
+        console.log(checked)
     }
 
     function generateCurrentCommentaryHistory() {
@@ -141,6 +182,33 @@ export default function ListComponent(props: { title: JSX.Element, intro: JSX.El
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={() => handleDialogClose()} autoFocus>
+                        Entendido
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog
+                open={openDialog2}
+                onClose={handleDialogClose2}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {dialogTitle2}
+                </DialogTitle>
+                <DialogContent>
+                    <Grid item
+                          padding={'5px'}
+                    >
+                        <Typography variant="body1" color="text.primary">
+                            {dialogMessage2}
+                        </Typography>
+                        <List>
+                            {commentaryRecords}
+                        </List>
+                    </Grid>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => handleDialogClose2()} autoFocus>
                         Entendido
                     </Button>
                 </DialogActions>
